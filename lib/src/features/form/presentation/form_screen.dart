4d716bdf92779471878d1c8adb2ad0d2280ef757
Janos85 <janosbalogh85@gmail.com>
2024-05-23 16:20:08 +0200

    ready

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -11,33 +11,38 @@ class FormScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Form(
-          child: Column(children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Email"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Form(
+            child: Column(children: [
+              const SizedBox(
+                height: 50,
               ),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: validateEmail,
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Passwort"),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Email"),
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: validateEmail,
               ),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: validatePw,
-            ),
-            const SizedBox(height: 32),
-            FilledButton(
-              onPressed: () {},
-              child: const Text("Login"),
-            ),
-          ]),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Passwort"),
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: validatePw,
+              ),
+              const SizedBox(height: 32),
+              FilledButton(
+                onPressed: () {},
+                child: const Text("Login"),
+              ),
+            ]),
+          ),
         ),
       ),
     );
@@ -45,11 +50,30 @@ class FormScreen extends StatelessWidget {
 
   String? validateEmail(String? input) {
     // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return "email can not be empty";
+    }
+    if (input.length < 6) {
+      return "email must be longer than 6 charachter";
+    }
+    if (!input.contains("@")) {
+      return "email must have \"@\" in it";
+    }
+    if (!(input.endsWith(".com") || input.endsWith(".de"))) {
+      return 'email must contain ".com" or".de"';
+    }
+    {}
     return null;
   }
 
   String? validatePw(String? input) {
     // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return "pasword can´t be empty ";
+    }
+    if (input.length < 6 || input.length > 12) {
+      return "Password need to be betwine 6-12 charachters";
+    }
     return null;
   }
 }
